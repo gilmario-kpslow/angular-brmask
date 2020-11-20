da113ba7bd2b5cdcc4db9b507e722ba8714d3039
gilmario batista <gilmariosoftware@gmail.com>
2020-11-20 15:22:34 -0300

    GL Mask

--- projects/glmask/src/lib/glmask.module.ts
+++ projects/glmask/src/lib/glmask.module.ts
@@ -1,18 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MascaraPipe } from './pipes/mascara.pipe';
-import { GlCPFDirective } from './directives/cpf.directive';
-import { GlCNPJDirective } from './directives/cnpj.directive';
 import { GlMASKDirective } from './directives/gl-mask.directive';
 
 @NgModule({
-  declarations: [MascaraPipe, GlCPFDirective, GlCNPJDirective, GlMASKDirective],
+  declarations: [MascaraPipe, GlMASKDirective],
   imports: [
     CommonModule
   ],
-  exports: [MascaraPipe, GlCPFDirective, GlCNPJDirective, GlMASKDirective],
+  exports: [MascaraPipe, GlMASKDirective],
   providers: [
-    GlCPFDirective, GlCNPJDirective, GlMASKDirective
+    GlMASKDirective
   ]
 })
 export class GlmaskModule { }
--- projects/glmask/src/public-api.ts
+++ projects/glmask/src/public-api.ts
@@ -1,4 +1,6 @@
 /*
- * Public API Surface of glmask
- */
+* Public API Surface of glmask
+*/
 export * from './lib/glmask.module';
+export { GlMASKDirective } from './lib/directives/gl-mask.directive';
+export { MascaraPipe } from './lib/pipes/mascara.pipe';

bad5e25ef7b9c2ca00004a16f8cc1a1dec540696
gilmario batista <gilmariosoftware@gmail.com>
2020-12-14 14:51:19 -0300

    Correçao

--- projects/glmask/src/public-api.ts
+++ projects/glmask/src/public-api.ts
@@ -5,3 +5,4 @@ export * from './lib/glmask.module';
 export { GlMASKDirective } from './lib/directives/gl-mask.directive';
 export { MascaraPipe } from './lib/pipes/mascara.pipe';
 export { GlFoneDirective } from './lib/directives/gl-mask-fone.directive';
+export { MascaraFonePipe } from './lib/pipes/mascara-fone.pipe';
